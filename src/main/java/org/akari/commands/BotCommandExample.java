a1e4f206b28f328c071cb1b30ffe5c63c72ed0e7
Akari_m <58370835+Akari-my@users.noreply.github.com>
2024-02-29 01:03:54 +0100

    Update BotCommandExample.java

--- src/main/java/org/akari/commands/BotCommandExample.java
+++ src/main/java/org/akari/commands/BotCommandExample.java
@@ -2,12 +2,13 @@ package org.akari.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 
 public class BotCommandExample extends ListenerAdapter {
 
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (event.getName().equals("ping")) { // Check if the command name is "ping"
             event.getHook().sendMessage("Pong!").queue(); // Send the message "Pong!"
         }
